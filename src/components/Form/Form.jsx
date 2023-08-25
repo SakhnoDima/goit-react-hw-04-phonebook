d84d63d602950c6084c56abc7b4819aab95f90ea
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-08-25 16:01:00 +0300

    123

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Formik, ErrorMessage } from 'formik';
 import { object, string } from 'yup';
